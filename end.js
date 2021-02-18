d00e9b9b24a3ad5dba89241f5966aefed9adde12
keerthanaAnandan <keerthanaanandanak@gmail.com>
2021-02-18 19:59:12 +0530

    initial

---
+++ end.js
@@ -0,0 +1,222 @@
+let passive = sessionStorage.getItem("passive");
+let aggressive = sessionStorage.getItem("aggressive");
+let assertive = sessionStorage.getItem("assertive");
+let passiveaggressive = sessionStorage.getItem("passiveaggressive");
+
+let mess;
+let typo;
+//passiveaggresive
+if (
+  passiveaggressive > aggressive &&
+  passiveaggressive > assertive &&
+  passiveaggressive > passive
+) {
+  typo = "Your communication style is Passive Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Struggles with accountability",
+    "Harbors their emotions, but speak as if everything is fine",
+    "Besides, your main intention when you take to IG is to be entertained, not necessarily to interact. This doesn’t mean you don’t like to stay up-to-date on what other people are doing. Of course you do! It’s just that you have no interest in showing others what you're doing, which is why your account is probably private at this moment.",
+    "Most like to communicate non-verbally",
+  ];
+}
+
+//aggressive
+else if (
+  aggressive > passive &&
+  aggressive > assertive &&
+  aggressive > passiveaggressive
+) {
+  typo = "Your communication style is: Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Try to remain in control",
+    "Speak with the intent to do harm or hurt their partners feelings.",
+    "Can be intimidating",
+    "Often speak in a condescending manner",
+  ];
+}
+//assertive
+else if (
+  assertive > aggressive &&
+  assertive > passive &&
+  assertive > passiveaggressive
+) {
+  typo = "Your communication style is Assertive:";
+  mess = [
+    "These type of communicators:",
+    "Understands the needs of their partners",
+    "Can set clear boundaries",
+    "Confident in the ability to manage conflict",
+    "Can speak clearly and in a manner that is understood",
+  ];
+}
+//passive
+else if (
+  passive > aggressive &&
+  passive > assertive &&
+  passive > passiveaggressive
+) {
+  typo = "Your communication style is Passive:";
+  mess = [
+    "These type of communicators:",
+    "Can easily shut down",
+    "Avoids expressing their needs",
+    "Has their guard up most times (maybe due to fear, past breakups)",
+    "May ignore your own personal needs, and sometimes place others’ ahead of yours.",
+  ];
+}
+//passive === assertive
+else if (
+  assertive == passive &&
+  assertive > aggressive &&
+  assertive > passiveaggressive
+) {
+  typo = "Your communication style is Assertive:";
+  mess = [
+    "These type of communicators:",
+    "Understands the needs of their partners",
+    "Can set clear boundaries",
+    "Confident in the ability to manage conflict",
+    "Can speak clearly and in a manner that is understood",
+  ];
+}
+//passive === aggressive
+else if (
+  passive == aggressive &&
+  passive > assertive &&
+  passive > passiveaggressive
+) {
+  typo = "Your communication style is Passive Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Struggles with accountability",
+    "Harbors their emotions, but speak as if everything is fine",
+    "Besides, your main intention when you take to IG is to be entertained, not necessarily to interact. This doesn’t mean you don’t like to stay up-to-date on what other people are doing. Of course you do! It’s just that you have no interest in showing others what you're doing, which is why your account is probably private at this moment.",
+    "Most like to communicate non-verbally",
+  ];
+}
+//passive === passiveaggressive
+else if (
+  passive == passiveaggressive &&
+  passive > assertive &&
+  passive > aggressive
+) {
+  typo = "Your communication style is Passive Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Struggles with accountability",
+    "Harbors their emotions, but speak as if everything is fine",
+    "Besides, your main intention when you take to IG is to be entertained, not necessarily to interact. This doesn’t mean you don’t like to stay up-to-date on what other people are doing. Of course you do! It’s just that you have no interest in showing others what you're doing, which is why your account is probably private at this moment.",
+    "Most like to communicate non-verbally",
+  ];
+}
+
+//aggressive === assertive
+else if (
+  aggressive === assertive &&
+  assertive > passive &&
+  assertive > passiveaggressive
+) {
+  typo = "Your communication style is: Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Try to remain in control",
+    "Speak with the intent to do harm or hurt their partners feelings.",
+    "Can be intimidating",
+    "Often speak in a condescending manner",
+  ];
+}
+
+//pasive = ass = agg == passaggg
+else if (
+  assertive == passiveaggressive &&
+  assertive == passive &&
+  assertive == aggressive &&
+  passiveaggressive == aggressive &&
+  passiveaggressive == passive &&
+  passive == aggressive
+) {
+  typo = "Your communication style is: Aggressive";
+  mess = [
+    "These type of communicators:",
+    "Try to remain in control",
+    "Speak with the intent to do harm or hurt their partners feelings.",
+    "Can be intimidating",
+    "Often speak in a condescending manner",
+  ];
+}
+
+const mao = mess.map((ms) => `<li>` + ms + `</li>`).join("");
+
+document.querySelector(".headmsg").innerHTML = typo;
+const msgme = document.querySelector(".msghere");
+msgme.className = "center-item";
+//document.querySelector(".msghere").innerHTML = mao;
+
+msgme.innerHTML = mao;
+document.querySelector(".usertype").value = typo;
+
+document.querySelector(".usertype").style.display = "none";
+
+//share buttons
+/* 
+
+Social Share Links:
+
+WhatsApp:
+https://wa.me/?text=[post-title] [post-url]
+
+Facebook:
+https://www.facebook.com/sharer.php?u=[post-url]
+
+Twitter:
+https://twitter.com/share?url=[post-url]&text=[post-title]
+
+Pinterest:
+https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title]
+
+LinkedIn:
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+*/
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+
+const linkedinBtn = document.querySelector(".linkedin-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+
+function init() {
+  let postUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI("Checkout what type of intagrammer I am ");
+  // let postImg = encodeURI(pinterestImg.src);
+
+  facebookBtn.setAttribute(
+    "href",
+    `https://www.facebook.com/sharer.php?u=${postUrl}`
+  );
+
+  twitterBtn.setAttribute(
+    "href",
+
+    `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+  );
+
+  // linkedinBtn.setAttribute(
+  // "href",
+  //"http://www.linkedin.com/shareArticle?mini=true&amp;url=https://simplesharebuttons.com"
+  //`https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  //);
+  linkedinBtn.setAttribute(
+    "href",
+    `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  );
+
+  whatsappBtn.setAttribute(
+    "href",
+    `https://wa.me/?text=${postTitle} ${postUrl}`
+  );
+}
+
+init();
---
+++ site.js
@@ -0,0 +1,171 @@
+let questions = [
+  {
+    id: 1,
+    question: "I can be very blunt when talking to my partner.",
+    answer: "passive",
+    options: ["Yes ", "No"],
+  },
+  {
+    id: 2,
+    question:
+      "I lie to avoid an argument (say nothing is wrong, when something really is)",
+    answer: "2",
+    options: ["Yes", "No"],
+  },
+  {
+    id: 3,
+    question: "I often struggle with taking accountability for my actions.",
+    answer: "2",
+    options: ["Yes", "No"],
+  },
+  {
+    id: 4,
+    question: "Sometimes the way I communicate hurts my partner.",
+    answer: "2",
+    options: ["Yes ", "No"],
+  },
+  {
+    id: 5,
+    question: "My honesty comes across in a very appropriate manner.",
+    answer: "2",
+    options: ["Yes  ", "No"],
+  },
+  {
+    id: 6,
+    question:
+      "I take time to understand my partner’s emotions and not just my own.",
+    answer: "2",
+    options: ["Yes  ", "No"],
+  },
+  {
+    id: 7,
+    question: "I am always right",
+    answer: "2",
+    options: ["Yes ", "No"],
+  },
+  {
+    id: 8,
+    question: "Sometimes I rather bury my head in the sand than speak my mind.",
+    answer: "2",
+    options: ["Yes", "No"],
+  },
+  {
+    id: 9,
+    question:
+      "I can sometimes be indirect (speaking under my breath, making backhanded, sarcastic comments)",
+    answer: "2",
+    options: ["Yes   ", "No"],
+  },
+  {
+    id: 10,
+    question: " Do you repeatedly make excuses to avoid certain people?",
+    answer: "2",
+    options: ["Yes   ", "No"],
+  },
+  {
+    id: 11,
+    question:
+      "Do you communicate more non-verbally (body language) than verbally? ",
+    answer: "2",
+    options: ["Yes   ", "No"],
+  },
+  {
+    id: 12,
+    question:
+      " I know how to say no and have established healthy boundaries in my life.",
+    answer: "2",
+    options: ["Yes  ", "No"],
+  },
+];
+
+let question_count = 0;
+//let points = 0;
+Max_Questions = 12;
+
+let passivePoints = 0;
+let aggressivePoints = 0;
+let assertivePoints = 0;
+let passiveagressivePoints = 0;
+
+window.onload = function () {
+  show(question_count);
+};
+
+function next(e) {
+  // if the question is last then redirect to final page
+  if (question_count == questions.length - 1) {
+    location.href = "end.html";
+  }
+
+  let user_answer = document.querySelector(".option.active").innerHTML;
+
+  //check answer
+  //passive(0)
+  if (user_answer === "Yes") {
+    passivePoints += 1;
+  }
+  //aggressive(1)
+  else if (user_answer === "Yes ") {
+    aggressivePoints += 1;
+  }
+  //assertive(2)
+  else if (user_answer === "Yes  ") {
+    assertivePoints += 1;
+  }
+  //passiveaggressive(3)
+  else if (user_answer === "Yes   ") {
+    passiveagressivePoints += 1;
+  }
+
+  sessionStorage.setItem("passive", passivePoints);
+  sessionStorage.setItem("aggressive", aggressivePoints);
+  sessionStorage.setItem("assertive", assertivePoints);
+  sessionStorage.setItem("passiveaggressive", passiveagressivePoints);
+  //let coloranswer = document.querySelector("li.option.active");
+  //e.target.coloranswer.classList.add("correct");
+
+  question_count++;
+  show(question_count);
+  toggleActive();
+  //Update the progress Bar
+
+  const progressBarFull = document.getElementById("progressBarFull");
+
+  progressBarFull.style.width = ` ${(question_count / Max_Questions) * 100}%`;
+}
+
+function show(count) {
+  //progress text
+  const progressText = document.getElementById("progressText");
+
+  progressText.innerText = `Question ${count + 1}/${Max_Questions}`;
+
+  let question = document.getElementById("questions");
+  let [first, second] = questions[count].options;
+  ///////no of questions
+
+  question.innerHTML = `
+  <h2>Q${count + 1}. ${questions[count].question}</h2>
+   <ul class="option_group">
+  <li><span class="option">${first}</span></li>
+  <li ><span class="option">${second}</span></li>
+ 
+</ul> 
+  `;
+  toggleActive();
+}
+
+function toggleActive() {
+  let option = document.querySelectorAll(".option");
+
+  for (let i = 0; i < option.length; i++) {
+    option[i].onclick = function () {
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].classList.contains("active")) {
+          option[i].classList.remove("active");
+        }
+      }
+      option[i].classList.add("active");
+    };
+  }
+}
